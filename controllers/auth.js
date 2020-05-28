8e9a84da82856b8a74339c19ae39212abcf4a3ff
oscarzapi <33022807+oscarzapi@users.noreply.github.com>
2020-05-28 12:01:09 +0400

    First commit

---
+++ controllers/auth.js
@@ -0,0 +1,64 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const expressJwt = require("express-jwt");
+const User = require("../models/user");
+
+exports.signup = async (req, res) => {
+  const userExists = await User.findOne({ email: req.body.email });
+  if (userExists)
+    return res.status(403).json({
+      error: "Email already exists",
+    });
+  const user = await new User(req.body);
+  await user.save();
+  res
+    .status(200)
+    .json({ message: "Signup process is successfull. Please sign in!" });
+};
+
+exports.signin = (req, res) => {
+  // find the user based on email
+  const { email, password } = req.body;
+
+  User.findOne({ email }, (err, user) => {
+    // if error or no user
+    if (err || !user) {
+      return res.status(401).json({
+        error: "User with that email does not exist. Please signin.",
+      });
+    }
+    // if user is found make sure the email and password match
+    // create auth method in user model an use here
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: "Email and password do not match",
+      });
+    }
+
+    // generate a token with user id and secret
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+
+    // persist the token as "t" in cockie with expiry date
+    res.cookie("t", token, { expire: new Date() + 9999 });
+
+    // return response with user and token to frontend client
+    const { _id, name, email } = user;
+    return res.json({
+      token,
+      user: { _id, email, name },
+    });
+  });
+};
+
+exports.signout = (req, res) => {
+  // clearing the cookie signs out the user
+  res.clearCookie("t");
+  return res.json({ message: "Signout success" });
+};
+
+exports.requireSignin = expressJwt({
+  // if token is valid, express jwt appends the verified users id
+  // in a auth key to the request object
+  secret: process.env.JWT_SECRET,
+  userProperty: "auth",
+});
